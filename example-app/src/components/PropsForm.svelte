4dc3383eeb0eb60e54581b5f30910b006ea839f6
Teemu Koivisto <tkoivisto456@gmail.com>
2021-08-28 13:12:33 +0300

    Add PropsForm to change TreeView props

---
+++ example-app/src/components/PropsForm.svelte
@@ -0,0 +1,72 @@
+<script>
+  export let leftIndent,
+    showLogButton,
+    showCopyButton,
+    valueComponent,
+    recursionOpts,
+    valueFormatter,
+    theme
+</script>
+
+<fieldset class="flex border-2 p-2 text-sm">
+  <legend class="px-2 text-0A text-base">Available props</legend>
+  <div class="m-2 mt-0 w-1/4">
+    <div class="field">
+      <label for="leftIndent">leftIndent?</label>
+      <input id="leftIndent" class="w-20 pl-1 bg-01 text-0B" bind:value={leftIndent}/>
+    </div>
+    <div class="field">
+      <label for="showLogButton">showLogButton?</label>
+      <input id="showLogButton" type="checkbox" bind:checked={showLogButton}/>
+    </div>
+    <div class="field">
+      <label for="showCopyButton">showCopyButton?</label>
+      <input id="showCopyButton" type="checkbox" bind:checked={showCopyButton}/>
+    </div>
+    <div class="field">
+      <label>valueComponent?</label>
+      <button class="btn-example">example</button>
+    </div>
+    <div class="field">
+      <label>recursionOpts?</label>
+      <button class="btn-example">example</button>
+    </div>
+    <div class="field">
+      <label>valueFormatter?</label>
+      <button class="btn-example">example</button>
+    </div>
+  </div>
+  <div class="flex flex-col w-3/4 mr-2 mb-2">
+    <div>
+      <label for="theme">theme?</label>
+      <a target="_blank" href="https://github.com/reduxjs/redux-devtools/tree/75322b15ee7ba03fddf10ac3399881e302848874/src/react/themes">examples</a>
+    </div>
+    <textarea id="theme" class="w-full h-full bg-01 text-0B" bind:value={theme}></textarea>
+  </div>
+</fieldset>
+
+<style>
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  input[type=number] {
+    -moz-appearance: textfield;
+  }
+  .field {
+    @apply flex items-center;
+  }
+  .field > label {
+    @apply mr-2;
+  }
+  .field + .field {
+    @apply mt-2;
+  }
+  .btn-example {
+    background-color: var(--tree-view-base02);
+    color: var(--tree-view-base0B);
+
+    @apply px-1 py-0.5 rounded-sm transition-colors duration-100 tracking-wide text-xs;
+  }
+</style>
--- example-app/src/routes/index.svelte
+++ example-app/src/routes/index.svelte
@@ -1,14 +1,43 @@
 <script>
   import TreeView from 'svelte-tree-view'
+  import PropsForm from '../components/PropsForm.svelte'
   import obj1 from './obj1.json'
 
-  let value = ''
-  let parsedJSON = obj1
   const placeholder = `Eg. {"a": 1, "b": [1,2,3]}`
+  let data = '',
+    parsedData = obj1,
+    leftIndent = '0.875em',
+    showLogButton = false,
+    showCopyButton = false,
+    valueComponent = undefined,
+    recursionOpts = {
+      shouldExpandNode: () => true,
+    },
+    valueFormatter = undefined,
+    theme = undefined,
+    parsedTheme = undefined
+
   $: {
-    if (value) {
+    if (data) {
       try {
-        parsedJSON = new Function(`return ${value}`)()
+        parsedData = new Function(`return ${data}`)()
+      } catch (e) {}
+    }
+  }
+  $: {
+    if (leftIndent && typeof document !== undefined) {
+      try {
+        document.documentElement.style.setProperty(`--tree-view-left-indent`, leftIndent)
+      } catch (e) {}
+    }
+  }
+  $: {
+    if (theme) {
+      try {
+        parsedTheme = new Function(`return ${theme}`)()
+        Object.keys(parsedTheme).forEach(key => {
+          document.documentElement.style.setProperty(`--tree-view-${key}`, parsedTheme[key])
+        })
       } catch (e) {}
     }
   }
@@ -19,17 +48,29 @@
     <a target="_blank" href="https://github.com/teemukoivisto/svelte-tree-view">svelte-tree-view</a>
   </h1>
   <p class="my-2">Copy-paste JSON objects to view them.</p>
+  <PropsForm
+    bind:leftIndent={leftIndent}
+    bind:showLogButton={showLogButton}
+    bind:showCopyButton={showCopyButton}
+    bind:valueComponent={valueComponent}
+    bind:recursionOpts={recursionOpts}
+    bind:valueFormatter={valueFormatter}
+    bind:theme={theme}
+  />
   <div class="my-4">
-    <button class="btn" on:click={() => (parsedJSON = obj1)}>Example 1</button>
+    <button class="btn" on:click={() => (parsedData = obj1)}>Example 1</button>
   </div>
   <div class="flex">
-    <textarea class="w-1/2 bg-0C text-01 p-2 border" bind:value {placeholder} />
+    <textarea class="w-1/2 bg-0C text-01 p-2 border" bind:value={data} {placeholder} />
     <TreeView
       class="tree-view w-1/2 px-4"
-      data={parsedJSON}
-      recursionOpts={{
-        shouldExpandNode: () => true,
-      }}
+      data={parsedData}
+      showLogButton={showLogButton}
+      showCopyButton={showCopyButton}
+      valueComponent={valueComponent}
+      recursionOpts={recursionOpts}
+      valueFormatter={valueFormatter}
+      theme={parsedTheme}
     />
   </div>
 </section>
