f3767e41e3ad627e8d5e86dfdbce7a26ba060ca2
Teemu Koivisto <tkoivisto456@gmail.com>
2021-10-15 13:57:10 +0300

    Fix cypress tests by disabling example-app server-side rendering

--- example-app/svelte.config.js
+++ example-app/svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
-const { DEPLOY_TO_GH } = process.env
+const { NO_SSR, DEPLOY_TO_GH } = process.env
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -10,6 +10,9 @@ export default {
   preprocess: preprocess(),
 
   kit: {
+    // Needed by Cypress tests as using ssr with `svelte-kit dev` might not hydrate the app fast enough,
+    // resulting in failing tests.
+    ...(NO_SSR && { ssr: false }),
     target: '#svelte',
     trailingSlash: 'never',
     paths: {
