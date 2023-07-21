fb1e68feb2e1c9cd9375496a5f82a203935baef4
Teemu Koivisto <tkoivisto456@gmail.com>
2023-07-21 11:23:12 +0300

    ci: remove unused code

--- packages/site/cypress/support/index.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-  interface Chainable {
-    noop: () => Cypress.Chainable<JQuery<HTMLElement>>
-  }
-}
