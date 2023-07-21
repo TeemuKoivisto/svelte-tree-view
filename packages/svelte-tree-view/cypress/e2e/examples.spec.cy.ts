da88148795b24fed0b3b04a0cfedd907fd807e00
Teemu Koivisto <tkoivisto456@gmail.com>
2023-07-21 10:56:49 +0300

    cy: add more wait

--- packages/svelte-tree-view/cypress/e2e/examples.spec.cy.ts
+++ packages/svelte-tree-view/cypress/e2e/examples.spec.cy.ts
@@ -10,7 +10,8 @@ describe('# UI', () => {
   it('Should render all 3 examples and inputted new data', () => {
     cy.visit('/')
     cy.get('.svelte-tree-view').find('li').should('have.length', 270)
-    cy.get('button').wait(2000).contains('Example 2').click()
+    cy.wait(4000)
+    cy.get('button').contains('Example 2').click()
     cy.get('.svelte-tree-view').find('li').should('have.length', 17)
     cy.get('button').contains('Example 3').click()
     cy.get('.svelte-tree-view').find('li').should('have.length', 126)
