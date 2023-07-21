cee7e45e8f5c0c9df80ca00bcfa8e6240a4c3735
Teemu Koivisto <tkoivisto456@gmail.com>
2023-07-21 11:14:05 +0300

    ci: increase wait to 3000

--- packages/site/cypress/e2e/examples.spec.cy.ts
+++ packages/site/cypress/e2e/examples.spec.cy.ts
@@ -10,7 +10,7 @@ describe('# UI', () => {
   it('Should render all 3 examples and inputted new data', () => {
     cy.visit('/', { failOnStatusCode: false })
     cy.get('.svelte-tree-view').find('li').should('have.length', 270)
-    cy.wait(2000)
+    cy.wait(3000)
     cy.get('button').contains('Example 2').click()
     cy.get('.svelte-tree-view').find('li').should('have.length', 17)
     cy.get('button').contains('Example 3').click()
