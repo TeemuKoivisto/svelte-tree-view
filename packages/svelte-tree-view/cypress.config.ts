11ebea5527262b37b3bd8352d0e61aebe1fb5708
Teemu Koivisto <tkoivisto456@gmail.com>
2025-07-08 20:17:14 +0300

    fix running cypress tests

--- packages/svelte-tree-view/cypress.config.ts
+++ packages/svelte-tree-view/cypress.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'cypress'
 
+let shouldSkip = false
+
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:5185',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        resetShouldSkipFlag() {
+          shouldSkip = false
+          return null
+        },
+        shouldSkip(value) {
+          if (value != null) shouldSkip = value
+          return shouldSkip
+        }
+      })
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--hide-scrollbars')
+          launchOptions.args.push('--high-dpi-support')
+          launchOptions.args.push('--window-size=1280,800')
+        }
+        return launchOptions
+      })
     }
   }
 })
