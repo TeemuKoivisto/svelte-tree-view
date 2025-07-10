7bb14aabe87126cb9fb3c70e5d16165f9ecf80ef
Teemu Koivisto <tkoivisto456@gmail.com>
2025-07-10 21:38:29 +0300

    remove RootNode, restore setting theme by props

--- packages/svelte-tree-view/src/lib/TreeView.svelte
+++ packages/svelte-tree-view/src/lib/TreeView.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { setContext, untrack } from 'svelte'
+  import { onMount, setContext, untrack } from 'svelte'
   import { get } from 'svelte/store'
 
-  import RootNode from './RootNode.svelte'
+  import TreeViewNode from './TreeViewNode.svelte'
   import { createPropsStore, createRootElementStore, createTreeStore } from './stores'
 
   import type { HTMLAttributes } from 'svelte/elements'
@@ -37,15 +37,21 @@
     valueFormatter,
     restProps: rest
   }
+  let rootElement: HTMLElement
   const propsStore = createPropsStore(propsObj)
   const rootElementStore = createRootElementStore()
   const treeStore = createTreeStore(propsStore)
   const newRecOpts = $derived({ ...DEFAULT_RECURSION_OPTS, ...recursionOpts })
+  const rootNode = treeStore.tree
 
   setContext<Stores>('svelte-tree-view', {
     propsStore,
     rootElementStore,
     treeStore
+  })
+
+  onMount(() => {
+    rootElementStore.set(rootElement)
   })
 
   $effect(() => {
@@ -78,14 +84,38 @@
     })
   })
 
-  // $effect(() => {
-  //   const newRecOpts = untrack(() => recursionOpts)
-  //   treeStore.update(
-  //     untrack(() => data),
-  //     newRecOpts,
-  //     $currentRecOpts?.shouldExpandNode !== newRecOpts.shouldExpandNode
-  //   )
-  // })
+  $effect(() => {
+    for (const key in theme) {
+      const value = theme[key as keyof typeof theme]
+      if (rootElement && key.includes('base') && value) {
+        rootElement.style.setProperty(`--tree-view-${key}`, value)
+      }
+    }
+  })
 </script>
 
-<RootNode />
+<ul {...rest} class={`${rest.class || ''} svelte-tree-view`} bind:this={rootElement}>
+  {#each $rootNode.children as child}
+    <TreeViewNode id={child.id} />
+  {/each}
+</ul>
+
+<style>
+  :root {
+    --tree-view-font-family: 'Helvetica Neue', 'Calibri Light', Roboto, sans-serif;
+    --tree-view-font-size: 13px;
+    --tree-view-left-indent: 0.875em;
+    --tree-view-line-height: 1.1;
+    --tree-view-key-margin-right: 0.5em;
+  }
+  ul {
+    background: var(--tree-view-base00);
+    font-family: var(--tree-view-font-family);
+    font-size: var(--tree-view-font-size);
+    height: max-content;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    width: max-content;
+  }
+</style>
